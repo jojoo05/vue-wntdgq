dfebfb5923202736af26c76788bf807df93e9944
jojoo05 <johanna.osterkorn@brgenns.ac.at>
2022-02-14 16:28:34 +0100

    1 file modified

--- src/components/Raten.vue
+++ src/components/Raten.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="raten">
     <h2>{{ masg }}</h2>
-    <input type="text" />
+    <input v-model.number="unserVariablenName" />
+    <h3>{{ unserVariablenName }}</h3>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Raten',
+  data: () => {
+    return {
+      unserVariablenName: 0,
+    };
+  },
   props: {
     msg: String,
   },
